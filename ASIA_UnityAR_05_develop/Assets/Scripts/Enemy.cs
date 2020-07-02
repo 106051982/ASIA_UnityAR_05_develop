28e136f64df8b13d79b2e21756faf14c8fa80570
muzhou0000 <55180633+muzhou0000@users.noreply.github.com>
2020-07-03 00:01:12 +0800

    v1

---
+++ ASIA_UnityAR_05_develop/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("速度"), Range(0, 10)]
+    public float speed = 1.5f;
+    [Header("停止距離"), Range(0,5)]
+    public float stopDistance = 2;
+    [Header("追蹤目標名稱")]
+    public string targetName = "111";
+    [Header("傷害"), Range(10, 100)]
+    public float damge;
+
+    private Transform target;
+    private NavMeshAgent nma;
+
+    private void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        nma.speed = speed;
+        nma.stoppingDistance = stopDistance;
+
+        target = GameObject.Find(targetName).transform;
+        nma.SetDestination(target.position);
+    }
+    private void Update()
+    {
+        Track();
+    }
+    void CreateEffect()
+    {
+        Destroy(gameObject);
+    }
+    void Track()
+    {
+        nma.SetDestination(target.position);
+
+        if(nma.remainingDistance<=stopDistance)
+         CreateEffect();
+        target.GetComponent<flowerpot>().Damage(damge);
+    }
+    void ClickAndDie()
+    {
+        CreateEffect();
+        flowerpot.target = transform;
+    }
+    private void OnMouseDown()
+    {
+        ClickAndDie();
+    }
+}
---
+++ ASIA_UnityAR_05_develop/Assets/Scripts/flowerpot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class flowerpot : MonoBehaviour
+{
+    public static Transform target;
+    [Header("血量"), Range(100, 500)]
+    public float hp;
+    [Header("血條")]
+    public Image hpBar;
+    public GameObject final;
+    public Text textCount;
+
+    int count;
+    float hpMax;
+    private void Start()
+    {
+        hpMax = hp;
+    }
+    
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        hpBar.fillAmount = hp / hpMax;
+
+        if (hp == 0)
+            final.SetActive(true);
+        textCount.text = "被種進去的數量：" + count;
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene("develop");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
